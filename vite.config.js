fe29e205db63a52c87aeba15c57026bb36d9ed9a
Hayden <haydengalyeanbiz@gmail.com>
2024-07-28 16:35:52 -0400

    added vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [react()],
+	preview: {
+		port: process.env.PORT || 4173,
+		host: true,
+	},
+});
