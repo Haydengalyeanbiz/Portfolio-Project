429c97d5952fdabc604032e3da5f4c2f6c9f3302
Hayden <haydengalyeanbiz@gmail.com>
2024-09-29 11:53:09 -0400

    smooth carousel trainsition and added an image

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -2,16 +2,64 @@ import Reveal from '../Reveal/Reveal';
 import { motion } from 'framer-motion';
 import './Skills.css';
 
+// Array of badge URLs
+const badges = [
+	{
+		src: 'https://img.shields.io/badge/React-%2320232a.svg?style=for-the-badge&logo=react&logoColor=%2361DAFB',
+		alt: 'React Badge',
+	},
+	{
+		src: 'https://img.shields.io/badge/Redux-%23593d88.svg?style=for-the-badge&logo=redux&logoColor=white',
+		alt: 'Redux Badge',
+	},
+	{
+		src: 'https://img.shields.io/badge/JavaScript-%23F7DF1E.svg?style=for-the-badge&logo=javascript&logoColor=black',
+		alt: 'Javascript Badge',
+	},
+	{
+		src: 'https://img.shields.io/badge/Vite-%23646CFF.svg?style=for-the-badge&logo=vite&logoColor=white',
+		alt: 'Vite Badge',
+	},
+	{
+		src: 'https://img.shields.io/badge/HTML-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white',
+		alt: 'HTML Badge',
+	},
+	{
+		src: 'https://img.shields.io/badge/CSS-%231572B6.svg?style=for-the-badge&logo=css3&logoColor=white',
+		alt: 'CSS Badge',
+	},
+	{
+		src: 'https://img.shields.io/badge/Framer_Motion-%23000000.svg?style=for-the-badge&logo=framer&logoColor=white',
+		alt: 'Framer-Motion Badge',
+	},
+	{
+		src: 'https://img.shields.io/badge/Flask-%23000000.svg?style=for-the-badge&logo=flask&logoColor=white',
+		alt: 'Flask Badge',
+	},
+	{
+		src: 'https://img.shields.io/badge/Express-%23000000.svg?style=for-the-badge&logo=express&logoColor=white',
+		alt: 'Express Badge',
+	},
+	{
+		src: 'https://img.shields.io/badge/Sequelize-%2300c7b7.svg?style=for-the-badge&logo=sequelize&logoColor=white',
+		alt: 'Sequelize Badge',
+	},
+	{
+		src: 'https://img.shields.io/badge/Supabase-%233ECF8E.svg?style=for-the-badge&logo=supabase&logoColor=white',
+		alt: 'Supabase Badge',
+	},
+];
+
 export const Skills = () => {
-	// Animation variants for seamless infinite looping carousel
+	// Animation variants for seamless looping
 	const carouselVariants = {
 		animate: {
-			x: ['0%', '-50%'], // Move from 0% to -50% (half the width to loop seamlessly)
+			x: ['0%', '-100%'], // Scroll from 0% to -100%
 			transition: {
 				x: {
 					repeat: Infinity,
 					repeatType: 'loop',
-					duration: 15,
+					duration: 20, // Adjust the speed here
 					ease: 'linear',
 				},
 			},
@@ -19,7 +67,7 @@ export const Skills = () => {
 	};
 
 	return (
-		<motion.div className='skills-wrapper'>
+		<div className='skills-wrapper'>
 			<Reveal>
 				<h2>My Skills</h2>
 			</Reveal>
@@ -29,109 +77,23 @@ export const Skills = () => {
 					variants={carouselVariants}
 					animate='animate'
 				>
-					<div className='skills-group'>
+					{badges.map((badge, index) => (
 						<img
-							src='https://img.shields.io/badge/React-%2320232a.svg?style=for-the-badge&logo=react&logoColor=%2361DAFB'
-							alt='React Badge'
+							key={index}
+							src={badge.src}
+							alt={badge.alt}
 						/>
+					))}
+					{/* Duplicate the badges for seamless looping */}
+					{badges.map((badge, index) => (
 						<img
-							src='https://img.shields.io/badge/Redux-%23593d88.svg?style=for-the-badge&logo=redux&logoColor=white'
-							alt='Redux Badge'
+							key={index + badges.length}
+							src={badge.src}
+							alt={badge.alt}
 						/>
-						<img
-							src='https://img.shields.io/badge/JavaScript-%23F7DF1E.svg?style=for-the-badge&logo=javascript&logoColor=black'
-							alt='Javascript Badge'
-						/>
-						<img
-							src='https://img.shields.io/badge/Vite-%23646CFF.svg?style=for-the-badge&logo=vite&logoColor=white'
-							alt='Vite Badge'
-						/>
-						<img
-							src='https://img.shields.io/badge/HTML-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white'
-							alt='HTML Badge'
-						/>
-						<img
-							src='https://img.shields.io/badge/CSS-%231572B6.svg?style=for-the-badge&logo=css3&logoColor=white'
-							alt='CSS Badge'
-						/>
-						<img
-							src='https://img.shields.io/badge/Framer_Motion-%23000000.svg?style=for-the-badge&logo=framer&logoColor=white'
-							alt='Framer-Motion Badge'
-						/>
-						<img
-							src='https://img.shields.io/badge/Flask-%23000000.svg?style=for-the-badge&logo=flask&logoColor=white'
-							alt='Flask Badge'
-						/>
-						<img
-							src='https://img.shields.io/badge/FlaskForms-%23000000.svg?style=for-the-badge&logo=flask&logoColor=white'
-							alt='FlaskForms Badge'
-						/>
-						<img
-							src='https://img.shields.io/badge/Express-%23000000.svg?style=for-the-badge&logo=express&logoColor=white'
-							alt='Express Badge'
-						/>
-						<img
-							src='https://img.shields.io/badge/Sequelize-%2300c7b7.svg?style=for-the-badge&logo=sequelize&logoColor=white'
-							alt='Sequelize Badge'
-						/>
-						<img
-							src='https://img.shields.io/badge/Supabase-%233ECF8E.svg?style=for-the-badge&logo=supabase&logoColor=white'
-							alt='Supabase Badge'
-						/>
-					</div>
-					{/* Duplicate set of badges to make it seamless */}
-					<div className='skills-group'>
-						<img
-							src='https://img.shields.io/badge/React-%2320232a.svg?style=for-the-badge&logo=react&logoColor=%2361DAFB'
-							alt='React Badge'
-						/>
-						<img
-							src='https://img.shields.io/badge/Redux-%23593d88.svg?style=for-the-badge&logo=redux&logoColor=white'
-							alt='Redux Badge'
-						/>
-						<img
-							src='https://img.shields.io/badge/JavaScript-%23F7DF1E.svg?style=for-the-badge&logo=javascript&logoColor=black'
-							alt='Javascript Badge'
-						/>
-						<img
-							src='https://img.shields.io/badge/Vite-%23646CFF.svg?style=for-the-badge&logo=vite&logoColor=white'
-							alt='Vite Badge'
-						/>
-						<img
-							src='https://img.shields.io/badge/HTML-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white'
-							alt='HTML Badge'
-						/>
-						<img
-							src='https://img.shields.io/badge/CSS-%231572B6.svg?style=for-the-badge&logo=css3&logoColor=white'
-							alt='CSS Badge'
-						/>
-						<img
-							src='https://img.shields.io/badge/Framer_Motion-%23000000.svg?style=for-the-badge&logo=framer&logoColor=white'
-							alt='Framer-Motion Badge'
-						/>
-						<img
-							src='https://img.shields.io/badge/Flask-%23000000.svg?style=for-the-badge&logo=flask&logoColor=white'
-							alt='Flask Badge'
-						/>
-						<img
-							src='https://img.shields.io/badge/FlaskForms-%23000000.svg?style=for-the-badge&logo=flask&logoColor=white'
-							alt='FlaskForms Badge'
-						/>
-						<img
-							src='https://img.shields.io/badge/Express-%23000000.svg?style=for-the-badge&logo=express&logoColor=white'
-							alt='Express Badge'
-						/>
-						<img
-							src='https://img.shields.io/badge/Sequelize-%2300c7b7.svg?style=for-the-badge&logo=sequelize&logoColor=white'
-							alt='Sequelize Badge'
-						/>
-						<img
-							src='https://img.shields.io/badge/Supabase-%233ECF8E.svg?style=for-the-badge&logo=supabase&logoColor=white'
-							alt='Supabase Badge'
-						/>
-					</div>
+					))}
 				</motion.div>
 			</div>
-		</motion.div>
+		</div>
 	);
 };
